51a98abe4f0221756a2e9693618d124c4fd9773f
Aditya Chandra <adityachandra9898@gmail.com>
2021-10-10 23:14:56 +0530

    Solution to Leetcode-1326

--- LeetCode/1326_minimum_number_of_taps_to_water_a_garden(Hard).cpp
+++ LeetCode/1326_minimum_number_of_taps_to_water_a_garden(Hard).cpp
@@ -1,59 +1,43 @@
-//Aditya Chandra
 #include <bits/stdc++.h>
-
-#define ll long long
-#define ld long double
-#define FOR(i, a, b, s) for (int i = (a); (s) > 0 ? i < (b) : i > (b); i += (s))
-#define for1(e) FOR(i, 0, e, 1)
-#define for2(i, e) FOR(i, 0, e, 1)
-#define for3(b, e) FOR(i, b, e, 1)
-#define for4(i, b, e) FOR(i, b, e, 1)
-#define EACH(x, a) for (auto& x : a)
-#define vi vector<ll>
-#define vvi vector<vector<ll>>
-#define pb push_back
-#define YES "YES"
-#define NO "NO"
-
 using namespace std;
 
 int minTaps(int n, vector<int>& ranges) {
-        vector<pair<int,int>> cutoffs(n+1);
-        for(int i = 0 ; i <= n ; i++){
-            cutoffs[i].first = i - ranges[i];
-            cutoffs[i].second = i + ranges[i];
+    vector<pair<int, int>> cutoffs(n + 1);
+    for (int i = 0; i <= n; i++) {
+        cutoffs[i].first = i - ranges[i]; //left range
+        cutoffs[i].second = i + ranges[i]; //right range
+    }
+    int currMin = 0;
+    int currMax = 0;
+    int ptr;
+    int cnt = 0;
+    while (currMax < n) {
+        for (int i = 0; i <= n; i++) {
+            if (cutoffs[i].first <= currMin && cutoffs[i].second > currMax) {
+                currMax = cutoffs[i].second;
+            }
         }
-        int currMin = 0;
-        int currMax = 0;
-        int ptr;
-        int cnt = 0;
-        while(currMax < n){
-                for(int i = 0 ; i <= n; i++){
-                    if(cutoffs[i].first <= currMin && cutoffs[i].second > currMax){
-                        currMax = cutoffs[i].second;
-                    }
-                }
-                if(currMin == currMax){
-                    return -1;
-                }
-                cnt++;
-                currMin = currMax;
+        if (currMin == currMax) {
+            return -1;
         }
-        return cnt;
-    }   
+        cnt++;
+        currMin = currMax;
+    }
+    return cnt;
+}
 
 int main() {
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
     cout.tie(NULL);
 
-    ll n;
+    int n;
     cin >> n;
     vector<int> ranges;
-    for1(n+1){
-        ll k;
+    for (int i = 0; i < n + 1; i++) {
+        int k;
         cin >> k;
-        ranges.pb(k);
+        ranges.push_back(k);
     }
     cout << minTaps(n, ranges) << endl;
 }
