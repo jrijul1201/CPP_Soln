859c0148f1177e07c5711bff063919f91a6c5b7f
Goldy-Singh <b20141@students.iitmandi.ac.in>
2021-10-11 16:24:07 +0530

    added solution to the URI problem 1007

---
+++ URI Online Judge/1007.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+ 
+using namespace std;
+ 
+int main() {
+ 
+    /**
+     * Escreva a sua solução aqui
+     * Code your solution here
+     * Escriba su solución aquí
+     */
+    int a,b,c,d,e;
+    cin>>a;
+    cin>>b;
+    cin>>c;
+    cin>>d;
+    e= (a*b)-(c*d);
+    cout<<"DIFERENCA = "<<e<<endl;
+    
+    return 0;
+}
